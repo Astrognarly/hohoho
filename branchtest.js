ebf3c55a39e55492f2d6c9dda22c1cee8703901a
Jihoon (JJ) Jung <Nnoitra5+GitHub@gmail.com>
2023-04-10 15:12:53 -0600

    Hello Kevin.

---
+++ branchtest.js
@@ -0,0 +1,16 @@
+function sayHello () {
+    console.log("Hello cruel world!");
+}
+
+sayHello ();
+
+function sayGoodbye () {
+    console.log("Goodbye cruel world!");
+}
+
+sayGoodbye ();
+
+let Kevin = "Kevin"
+sayHello = function () {
+    console.log ("Hello " + Kevin);
+}
